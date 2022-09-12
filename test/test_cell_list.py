d8ca944d7ca338da675578ff9d52ffa37164694d
fortrest-jr <30597575+fortrest-jr@users.noreply.github.com>
2022-09-12 13:37:14 +0500

    Update test_cell_list.py
    add little fixes for more clarity

--- test/test_cell_list.py
+++ test/test_cell_list.py
@@ -17,11 +17,12 @@ def refresh_and_get_contact_info(page, index):
 def test_get_contact_info_from_list(browser, index_to_get):
     cell_list_page = CellListPage(browser)
     cell_list_page.open()
+    
     cell_list_page.select_contact_from_list(index_to_get)
+    
     list_contact = cell_list_page.get_contact_from_list(index_to_get)
     info_contact = cell_list_page.get_contact_from_contact_info()
-
-    assert list_contact == info_contact
+    assert info_contact == list_contact
 
 
 @allure.description('Test updating info in selected item in list')
@@ -29,12 +30,13 @@ def test_get_contact_info_from_list(browser, index_to_get):
 def test_update_contact(browser, test_contact, index_to_update):
     cell_list_page = CellListPage(browser)
     cell_list_page.open()
-    cell_list_page.select_contact_from_list(index_to_update)
+    
+    cell_list_page.select_contact_from_list(index_to_update)    
     cell_list_page.fill_contact_info(test_contact)
     cell_list_page.update_contact()
+    
     list_contact = cell_list_page.get_contact_from_list(index_to_update)
     assert list_contact == test_contact
-
     info_contact = refresh_and_get_contact_info(cell_list_page, index_to_update)
     assert info_contact == test_contact
 
@@ -45,15 +47,13 @@ def test_create_full_contact(browser, test_contact):
     cell_list_page.open()
 
     old_total_length = cell_list_page.get_total_list_length()
-
     cell_list_page.fill_contact_info(test_contact)
     cell_list_page.create_contact()
+    
     assert (new_total_length := cell_list_page.get_total_list_length()) == old_total_length + 1
-
     cell_list_page.scroll_to_list_end()
     list_contact = cell_list_page.get_contact_from_list(new_total_length-1)
     assert list_contact == test_contact
-
     info_contact = refresh_and_get_contact_info(cell_list_page, new_total_length-1)
     assert info_contact == test_contact
 
@@ -62,8 +62,9 @@ def test_create_full_contact(browser, test_contact):
 def test_empty_contact_creation_attempt_do_not_increment_counter(browser):
     cell_list_page = CellListPage(browser)
     cell_list_page.open()
-
+    
     old_total_length = cell_list_page.get_total_list_length()
-
     cell_list_page.create_contact()
-    assert cell_list_page.get_total_list_length() == old_total_length
+    
+    new_total_length = cell_list_page.get_total_list_length()
+    assert new_total_length == old_total_length
